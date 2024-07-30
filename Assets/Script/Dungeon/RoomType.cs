8e81a8adb72debad7fc8e830373286633fffb6fa
hyleungtommy <104375225+hyleungtommy@users.noreply.github.com>
2024-07-29 23:04:25 -0400

    add new monsters

---
+++ Assets/Script/Dungeon/RoomType.cs
@@ -0,0 +1,17 @@
+namespace RPG{
+    public enum RoomType{
+        INVALID=0,
+        ONE_DOOR=1,
+        TWO_DOOR=2,
+        THREE_DOOR=3,
+        EMPTY=4,
+        BEFORE_BOSS=5,
+        BOSS=6,
+        MONSTER=7,
+        TRAP=8,
+        TREASURE=9,
+        MIMIC=10,
+        TREASURE_AND_MONSTER=11,
+        TRANSPORTATION_TRAP=12
+    }
+}
